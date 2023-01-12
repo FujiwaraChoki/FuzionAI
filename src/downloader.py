aa1a7c80b2a24a6088ca195905e673940220477d
Sami Hindi <78088687+FujiwaraChoki@users.noreply.github.com>
2023-01-12 22:15:41 +0100

    Update downloader.py

--- src/downloader.py
+++ src/downloader.py
@@ -1,17 +1,37 @@
 import random
-from pytube import Channel
+import requests
+from pytube import YouTube
+import os
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 
 
-def download_random_videos(channel_links):
-    '''Download a random video from channels.
-
-    Args:
-        channel_link: A link to a YouTube channel.
-    '''
-    for channel in channel_links:
-        channel = Channel(channel)
-        videos = channel.video_urls
+def download_random_videos(channel_urls):
+    # Download random videos from each channel
+    # First, get all videos from channel
+    for channel_url in channel_urls:
+        channel_id = channel_url.split('/')[-1]
+        key = ''
+        with open('.env.local', 'r') as f:
+            key = f.read().split('=')[1]
+        response = requests.get(
+            f'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={channel_id}&maxResults=50&order=date&type=video&key={key}')
+        # Get random video from channel
+        videos = response.json()['items']
         random_video = random.choice(videos)
-        # Download
-        random_video.download('build/')
+        video_id = random_video['id']['videoId']
+
+        # Download video with pytube
+        yt = YouTube(f'https://www.youtube.com/watch?v={video_id}')
+        yt.streams.filter(progressive=True, file_extension='mp4').order_by(
+            'resolution').desc().first().download('build')
+
+
+def combine_videos(final_name):
+    '''
+    Combine videos in the build directory into one video.
+    '''
+    video_clips = []
+    for video in os.listdir('build'):
+        video_clips.append(VideoFileClip(f'build/{video}'))
+    final_clip = concatenate_videoclips(video_clips)
+    final_clip.write_videofile(f'output/{final_name}.mp4')
